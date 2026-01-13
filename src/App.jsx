387d7f722379d4041ae2413baf98488c236c5c09
Munisbek Raimjonov <munisbekraimjonov33@gmail.com>
2026-01-13 16:41:39 +0500

    Backup local changes before GitHub sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
